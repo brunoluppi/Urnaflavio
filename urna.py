d67678fb0d082c2c1858b6b8497455998f4ae875
brunoluppi <luppibruno@gmail.com>
2020-07-06 17:39:12 -0300

    a

--- urna.py
+++ urna.py
@@ -21,122 +21,89 @@ prespass.close()
 admpass = open('admpass.txt', 'w')
 admpass.write("admtse")
 admpass.close()
-print("              Bem Vindo à Urna Eletrônica")
-print("")
-print("|                 escolha um usuario")
-print("")
-print("                Administrador digite 1")
-print("               secretario digite 2")
-print("               presidente digite 3")
-print("               mesario digite 4")
-print("")
-usuario=input()
-um="1"
-while usuario!=um:
-        print("somente o administrador tem cadastro")
-        print("")
-        print("              Bem Vindo à Urna Eletrônica")
-        print("")
-        print("|                 escolha um usuario")
-        print("-")
-        print("|                Administrador digite 1")
-        print("")
-        usuario=input()
-else:
-        admpass=open('admpass.txt','r')
-        senhadoadm=admpass.read()
-        admpass.close()
-        s=input("digite sua senha do administrador: ")
-        print()
-        while s!=senhadoadm:
-                print("senha incorreta")
-                s=str(input("digite sua senha: "))
-        else:
-                print ("seja bem vindo, administrador!")
-                print("")
-                print("|              para cadastrar secretário digite 1")
-                print("")
-                escolha=input()
-                um="1"
-                while escolha !=um:
-                        print("opção apenas para cadastro de secretario \n")
-                        print ("digite 1 para cadastrar o secretario")
-                        escolha=input()
-                else:
-                        print("digite uma nova senha para o secretario")
-                        senhasecretario=str(input())
-                        secpass=open('secpass.txt', 'w')
-                        secpass.write(senhasecretario)
-                        secpass.close()
-                        print("senha cadastrada com sucesso")
-                        print()
-                        print("")
-input("para sair do perfil de administrador aperte Enter")
-print("              Bem Vindo à Urna Eletrônica")
-print("")
-print("|                 escolha um usuario")
-print("")
-print("|                Administrador digite 1")
-if not(senhasecretario==""):
-        print("|                Secretário digite 2")
-if not(senhapresidente==""):
-        print("|                Presidente digite 3")
-if not(senhamesario==""):
-        print("|                  Mesario digite 4")
-print("")
-usuario=input()
-dois="2"
-while usuario!=dois:
-                print("administrador já cadastrou o secretário")
-                print("")
-                usuario=input("digite 2 para logar como secretario")                
-else :
-                print("digite sua senha de secretário")
-                ss=input()
-                secpass=open('secpass.txt','r')
-                senhasecretario=secpass.readline()
-                secpass.close()
-                while ss!=senhasecretario:
-                        print("senha incorreta")
-                        ss=str(input("digite sua senha: "))
 
-                else:
-                        print()
-                        print("seja bem vindo, secretário")
-                        print()
-                        print("cadastro presidente")
-                        print("digite a senha do presidente")
-                        SPRESIDENTE=str(input())
-                        prespass=open('prespass.txt', 'w')
-                        prespass.write(SPRESIDENTE)
-                        prespass.close()                                
-                        print("cadastro presidente realizado com sucesso")
-                        print()
-                        print("                         ")
-                        print("cadastro mesario")
-                        print("digite a senha do mesario")            
-                        SMESARIO=str(input())
-                        print("cadastro mesario realizado com sucesso")
-                        print("")
-                        print("cadastro candidatos")
-                        print("digite a quantidade de candidatos")
-                        NC=int(input())
-                        candidatos = {}
-                        LC=[]
-                        if NC != None:
-                                for i in range(int(NC)):
-                                    nome = input('Nome: ')
-                                    candidatos.update({
-                                        i: {
-                                            "name": nome.capitalize(),
-                                            "numEleitoral": random.randint(1,100),
-                                            "qtdVotos": 0
-                                        }
-                                    })
-                                    os.system('cls||clear')
-                                    for qtdCandidato in candidatos.values():
-                                        print('Candidato: {} \nNúmero eleitoral: {}\n'.format(qtdCandidato['name'], qtdCandidato['numEleitoral']))                             
-print("                                 ")
+user = int(input("Sistema Eleitoral / Urna Eleetronica \n  \n escolha um user \n  \n Administrador digite - 1 \n secretario digite - 2 \n presidente digite - 3 \n mesario digite - 4 \n : "))
+
+while user!=1:
+
+    user = int(input("Somente o ADMINSITRADOR tem cadastro \n Para administrador, digite 1"))
+
+else:
+    admpass=open('admpass.txt','r')
+    senhaadm=admpass.read()
+    admpass.close()
+    s=input("digite sua senha do administrador: ")
+    while s!=senhaadm:
+        print("senha incorreta")
+        s=str(input("digite sua senha: "))
+    else:
+        escolha = int(input('Seja bem vindo, administrador \n \n Para cadastrar secretário, digite 1 \n : '))
+        while escolha !=1:
+            escolha = int(input('Opção para cadastro de secretario \n digite 1 para cadastrar secretario \n : '))
+        else:
+            print("digite uma nova senha para o secretario")
+            senhasecretario=str(input())
+            secpass=open('secpass.txt', 'w')
+            secpass.write(senhasecretario)
+            secpass.close()
+            print("senha cadastrada com sucesso")
+            print("")
+
+input("pressione enter para sair do menu de administrador")
+print("Sistema Eleitoral / Urna Eleetronica \n ")
+print("escolha um user \n ")
+print("Administrador digite 1")
+if not(senhasecretario==""):
+        print("Secretário digite 2")
+if not(senhapresidente==""):
+        print("Presidente digite 3")
+if not(senhamesario==""):
+        print("Mesario digite 4")
+print("")
+user = int(input(": "))
+
+while user!=2:
+    print("administrador já cadastrou o secretário")
+    print("")
+    user=input("digite 2 para logar como secretario")                
+else :
+    print("digite sua senha de secretário")
+    ss=input()
+    secpass=open('secpass.txt','r')
+    senhasecretario=secpass.readline()
+    secpass.close()
+    while ss!=senhasecretario:
+        print("senha incorreta")
+        ss=str(input("digite sua senha: "))
+
+    else:
+
+        passpresidente=str(input("Bem vindo, secretario \n \n cadastro presidente \n digite a senha do presidente"))
+        prespass=open('prespass.txt', 'w')
+        prespass.write(passpresidente)
+        prespass.close()                                
+        print("cadastro presidente realizado com sucesso \n ")
+        print("cadastro mesario \n ")            
+        SMESARIO=str(input("digite a senha do mesario: "))
+        print("cadastro mesario realizado com sucesso \n ")
+        print("cadastro de candidatos")
+        NC=int(input("Número de candidatos: "))
+        candidatos = {}
+        LC=[]
+        if NC != None:
+            for i in range(int(NC)):
+                nome = input('Nome: ')
+                candidatos.update({
+                i: {
+                    "name": nome.capitalize(),
+                    "numEleitoral": random.randint(1,100),
+                    "qtdVotos": 0
+                    }
+                    })
+                os.system('cls||clear')
+                for qtdCandidato in candidatos.values():
+                    print('Candidato: {} \nNúmero eleitoral: {}\n'.format(qtdCandidato['name'], qtdCandidato['numEleitoral']))                             
+print("")
 print("cadastro eleitores")
 print("digite a quantidade de eleitores")
 SE=int(input())
@@ -147,64 +114,45 @@ while SE>0:
     LE.append(E)
     SE=SE-1
 else:
-    print("")
-print("")
-print("|             escolha um usuario")
-print("")
-print("|              Secretário digite 1")
-print("|              Presidente digite 2")
-print("|                Mesario digite 3")
-print("")
-usuario=int(input())
-while usuario!=2: 
-    print("é necessario que o presidente entre para zerar votos")
-    print("                                                             ")
-    print("|             escolha um usuario")
-    print("                                                     ")
-    print("              Secretário digite 1")
-    print("              Presidente digite 2")
-    print("               Mesario digite 3")
-    print("                                                     ")
-    usuario=int(input())    
+
+    user=int(input("\n escola um usuario \n \n Secretário digite 1 \n Presidente digite 2 \n Mesario digite 3 \n"))
+
+while user!=2: 
+
+    user=int(input("É preciso que o presidente entre para zerar a votação \n \n escolha um usuario \n \n Secretário digite 1 \n Presidente digite 2 \n Mesario digite 3 \n"))    
+
 else:
-    print("digite sua senha, senhor presidente")
-    sp=str(input())
-    while sp!=SPRESIDENTE:
+    sp=str(input("Digite a senha do presidente: "))
+    while sp!=passpresidente:
         print("senha incorreta")
         sp=str(input())
     else:
         QC=len(LC)
         (QC)
         votos=0
-        print("                                                         ")
-        print("bem-vindo senhor precidente")
-        print("                                                         ")
-        input("para zerar votaçao tecle enter")
-        input("votaçao zerar, tecle enter para sair")
-print("                                                                 ")
-print("             escolha um usuario")
-print("                                                                 ")
-print("              Secretário digite 1")
-print("              Presidente digite 2")
-print("                Mesario digite 3")
-print("                                                              ")
-usuario=int(input())
-while usuario!=3:
-    print("entre como mesario para iniciar a votaçao")
-    usuario=input()
+        input("\n Bem Vindo PRESIDENTE \n \n para zerar votaçao tecle enter")
+        input("votaçao zerada, tecle enter para sair")
+        
+user=int(input("\n escolha um usuario \n \n Secretário digite 1 \n Presidente digite 2 \n Mesario digite 3 \n"))
+
+while user!=3:
+
+    user=input("entre como mesario para iniciar a votaçao")
+
 else:
-    sm=input("digite sua senha de mesario: ")
+
+    sm=input("digite senha do mesario: ")
+
 while SMESARIO!=sm:
-    print("senha incorreta")
-    sm=input("digite a senha novamente: ")
+    
+    sm=input("senha incorreta \n digite a senha novamente: ")
+
 else:
-    print("                                       ")
-    print("bem-vindo mesario")
-    print("                                 ")
-    input("para iniciar votaçao tecle enter")
-print()
-print("para votar digite sua senha")
-se=input()
+
+    input("\n Bem Vindo mesario \n para iniciar votaçao tecle enter")
+    
+se=input("\n Digite a senha para iniciar a votação")
+
 while not se in LE:
     print()
     se=print("senha incorreta, digite novemente")
@@ -215,10 +163,8 @@ else:
     for i in LC:
         print(i)
         print("deseja votar em",i)
-        lol=int(input("se sim 1 se nao 2"))
-        if lol==2:
-            print("")
+        CV=int(input("se sim 1 se nao 2"))
+        if CV==2:
             input()
             print("voto computado")
-            print("erro")
 print("votaçao encerrada")
